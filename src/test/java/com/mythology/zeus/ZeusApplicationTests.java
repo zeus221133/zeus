fc8f2d05cab48f1d3fbe8c62d3ecd946e3e7c7ea
William Zheng <boyisxuxu@gmail.com>
2023-09-14 21:07:07 +0800

    init

---
+++ src/test/java/com/mythology/zeus/ZeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mythology.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
